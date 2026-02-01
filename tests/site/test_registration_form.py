19a2c94873eff9eb116482b3bdd84a816caa28c4
Dima <graiwers@gmail.com>
2026-02-01 16:23:13 +0400

    2 commit

---
+++ tests/site/test_registration_form.py
@@ -0,0 +1,41 @@
+
+import allure
+from selene import have, by
+
+from selene.support.shared import browser
+
+from tests.utils import attach
+
+
+@allure.title("Successful fill form")
+def test_successful():
+    # browser = setup_browser
+    first_name = "Иван"
+    last_name = "Иванов"
+
+    with allure.step("Open registrations form"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+        browser.driver.execute_script("$('footer').remove()")
+        browser.driver.execute_script("$('#fixedban').remove()")
+
+    with allure.step("Fill form"):
+        browser.element("#firstName").set_value(first_name)
+        browser.element("#lastName").set_value(last_name)
+        browser.element("#userEmail").set_value("random@mail.ru")
+        browser.element("#genterWrapper").element(by.text("Male")).click()
+        browser.element("#userNumber").set_value("8800123456")
+        browser.element("#currentAddress").set_value("Россия, г. Мытищи, Ленинская ул., д. 16 кв.194")
+        browser.element("#state").click()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element("#submit").click()
+
+    with allure.step("Check form results"):
+        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+
