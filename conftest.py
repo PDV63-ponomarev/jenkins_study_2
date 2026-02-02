ad8a83d1f905e8cf5b5d05b597b359dee7e4fa03
Dima <graiwers@gmail.com>
2026-02-02 15:32:48 +0400

    get env

--- conftest.py
+++ conftest.py
@@ -6,6 +6,13 @@ from selene import Browser, Config
 
 from utils import attach
 
+from dotenv import load_dotenv
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
@@ -18,8 +25,12 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
